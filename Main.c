190634394c4d795ab423eb7ea714b1e4a8b275c5
skek0 <77053845+skek0@users.noreply.github.com>
2024-09-27 21:45:56 +0900

    2024.09.27 게임 완료 및 반복, 색상

--- Main.c
+++ Main.c
@@ -15,9 +15,11 @@
 
 int maxX, maxY;
 int* board;
+int mineAmount;
 
 int isFirstHit = 1;
-int mineAmount;
+int play = 1;
+int failed = 0;
 
 int StartScreen();
 
@@ -30,7 +32,8 @@ int CheckNearby(int posX, int posY);
 void RenderBoard();
 int CheckCondition(int amount);
 void Mark(int posX, int posY);
-void Finished(int cleared);
+int Finished(int cleared);
+void Textcolor(int colorNum);
 
 void PrintAnswer();
 
@@ -40,99 +43,106 @@ int main()
 	{
 		return;
 	}
-	
-	system("cls");
-
-
-	// 가로 세로 입력받고 보드 생성
-	int sizeX, sizeY;
-	printf("size : ");
-	scanf_s("%d %d", &sizeX, &sizeY);
-	printf("\nmines : ");
-	scanf_s("%d", &mineAmount);
-
-	system("cls");
-	maxX = sizeX; maxY = sizeY;
-	BoardMaker(sizeX+2, sizeY+2);
-	//LayMines(mineAmount);
-	// 키 입력
-	char key = 0;
-
-	int posX = 1, posY = 1;
-
-	Position(posX, posY);
-
-	while (1)
+	while (play)
 	{
-		if (_kbhit())
-		{
-			key = _getch();
+		system("cls");
+		// 가로 세로 입력받고 보드 생성
+		int sizeX, sizeY;
+		Position(0, 0);
+		printf("size : ");
+		scanf_s("%d %d", &sizeX, &sizeY);
+		printf("\nmines : ");
+		scanf_s("%d", &mineAmount);
 
-			if (key == -32)
+		system("cls");
+		maxX = sizeX; maxY = sizeY;
+		BoardMaker(sizeX+2, sizeY+2);
+
+		// 키 입력
+		char key = 0;
+
+		int posX = 1, posY = 1;
+
+		Position(posX, posY);
+		
+
+		while (1)
+		{
+			if (_kbhit())
 			{
 				key = _getch();
+
+				if (key == -32)
+				{
+					key = _getch();
+				}
+
+				switch (key)
+				{
+				case UP:
+					if (posY > 1)
+					{
+						posY -= 1;
+					}
+					break;
+
+				case LEFT:
+					if (posX >= 2)
+					{
+						posX -= 2;
+					}
+					break;
+
+				case RIGHT:
+					if (posX <= maxX * 2 - 2)
+					{
+						posX += 2;
+					}
+					break;
+
+				case DOWN:
+					if (posY <= maxY - 1)
+					{
+						posY += 1;
+					}
+					break;
+
+				case SELECT:
+					Select((posX + 1) / 2, posY);
+					break;
+
+				case MARK:
+					if (!isFirstHit)
+					{
+						Mark((posX + 1) / 2, posY);
+					}
+					break;
+
+				default:
+					break;
+				}
+
+				system("cls");
+				if (CheckCondition(mineAmount) == 1)
+				{
+					play = Finished(1);
+					break;
+				}
+				else if (failed == 1)
+				{
+					play = Finished(0);
+					break;
+				}
+				RenderBoard();
 			}
-
-			switch (key)
-			{
-			case UP:
-				if (posY > 1)
-				{
-					posY -= 1;
-				}
-				break;
-
-			case LEFT:
-				if (posX >= 2)
-				{
-					posX -= 2;
-				}
-				break;
-
-			case RIGHT:
-				if (posX <= maxX*2 - 2)
-				{
-					posX += 2;
-				}
-				break;
-
-			case DOWN:
-				if (posY <= maxY-1)
-				{
-					posY += 1;
-				}
-				break;
-
-			case SELECT:
-				Select((posX + 1) / 2, posY);
-				break;
-
-			case MARK:
-				if (!isFirstHit)
-				{
-					Mark((posX + 1) / 2, posY);
-				}
-				break;
-
-			default:
-				break;
-			}
-
-			system("cls");
-			if (CheckCondition(mineAmount) == 1)
-			{
-				Finished(1);
-				break;
-			}
-			RenderBoard();
+			Position(0, maxY + 2);
+			PrintAnswer();
+			Position(posX, posY);
+			printf("▷");
 		}
-		Position(0,maxY+2);
-		PrintAnswer();
-		Position(posX, posY);
-		printf("★");
+		free(board);
 	}
 
-	free(board);
 
 	return 0;
 }
@@ -199,7 +209,6 @@ void Position(int x, int y)
 	// 콘솔 커서의 좌표를 설정하는 함수입니다.
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), position);
 }
-
 int CalculateIndex(int x, int y)
 {
 	int result = (maxX+2) * y + x;
@@ -289,7 +298,7 @@ void Select(int posX, int posY)
 		break;
 	case 12:
 		system("cls");
-		Finished(0);
+		failed = 1;
 		break;
 	}
 }
@@ -324,24 +333,29 @@ void RenderBoard()
 				switch (value)
 				{
 				case 10:
+					Textcolor(9);
 					printf("■ ");
 					break;
 				case 11:
 				case 12:
+					Textcolor(7);
 					printf("□ ");
 					break;
 				case 13:
 				case 14:
+					Textcolor(12);
 					printf("▣ ");
 					break;
 				case 15:
 				case 16:
+					Textcolor(15);
 					printf("? ");
 					break;
 				case 0:
 					printf("  ");
 					break;
 				default:
+					Textcolor(15);
 					printf("%d ", value);
 					break;
 				}
@@ -396,8 +410,48 @@ void Mark(int posX, int posY)
 	}
 }
 
-void Finished(int cleared)
+int Finished(int cleared)
 {
+	for (int j = 0; j < maxY + 2; j++)
+	{
+		for (int i = 0; i < maxX + 2; i++)
+		{
+			int value = board[CalculateIndex(i, j)];
+			switch(value){
+			case 10:
+				Textcolor(9);
+				printf("■ ");
+				break;
+			case 11:
+			case 15:
+			case 0:
+				printf("  ");
+				break;
+			case 13:
+				TextColor(12);
+				printf("X ");
+				break;
+			case 12:
+			case 14:
+			case 16:
+				Textcolor(12);
+				printf("◈ ");
+				break;
+			default:
+				Textcolor(15);
+				printf("%d ", value);
+				break;
+			}
+		}
+		printf("\n");
+	}
+
+	_getch();
+
+	system("cls");
+	Position(0, 0);
+	Textcolor(15);
+
 	if (cleared)
 	{
 		printf("CONGRATULATIONS!");
@@ -407,37 +461,62 @@ void Finished(int cleared)
 		printf("Failed..");
 	}
 
+	isFirstHit = 1;
+	failed = 0;
+
 	Position(2, 4);
 	printf("Restart");
 	Position(2, 5);
 	printf("Quit");
+
+	int key = 0;
+	int selected = 0, game = 1;
+
+	Position(0, 4);
+	printf("▶");
+
+	while (!selected)
+	{
+		if (_kbhit())
+		{
+			key = _getch();
+
+			if (key == -32)
+			{
+				key = _getch();
+			}
+
+			switch (key)
+			{
+			case UP:
+				Position(0, 5);
+				printf("  ");
+				Position(0, 4);
+				printf("▶");
+				game = 1;
+				break;
+			case DOWN:
+				Position(0, 4);
+				printf("  ");
+				Position(0, 5);
+				printf("▶");
+				game = 0;
+				break;
+			case SELECT:
+				selected = 1;
+				break;
+
+			default:
+				break;
+			}
+		}
+	}
+	return game;
 }
-
-/*
-	1. 시작과 끝
-	보드 크기, 폭탄 수 설정
-	~게임 중~
-	폭탄 클릭시 게임 오버
-	보드 내 클릭하지 않은 칸이 폭탄의 수와 같다면 클리어
-
-	2. 게임에서 필요한 기능
-	보드의 가로 세로를 받아 보드를 생성하기
-	WASD로 이동, J로 선택, K로 폭탄 마크와 ? 표기
-	"첫 선택시" 폭탄의 위치를 정하기
-	선택한 블럭의 인접한 8칸 내 폭탄의 총 갯수를 블럭의 위치에 대체
-	인접한 블럭 내 폭탄이 0개일시 인접한 모든 블록을 선택한것으로 간주
-	폭탄 선택시 게임오버
-	선택되지 않은 블럭의 수가 폭탄의 수와 같다면 승리
-
-	3. 필요한 기능을 추상화
-
-	4. 코드로 옮기기
-
-
-*/
 
 void PrintAnswer()
 {
+	Textcolor(15);
 	for (int j = 0; j < maxY+2; j++)
 	{
 		for (int i = 0; i < maxX+2; i++)
@@ -446,4 +525,8 @@ void PrintAnswer()
 		}
 		printf("\n");
 	}
+}
+void Textcolor(int colorNum)
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), colorNum);
 }
